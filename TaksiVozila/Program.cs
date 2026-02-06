8b009213ca8037beec6d1fde525cff1702c1163c
Andrija Gradištanac <andra200313@gmail.com>
2026-02-06 23:02:14 +0100

    TCP za taksi vozila

--- TaksiVozila/Program.cs
+++ TaksiVozila/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TaksiVozila
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("SERVER POKRENUT");
+
+            // TCP za taksi vozila
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 5000);
+            tcpListener.Start();
+            Console.WriteLine("TCP listener (vozila) na portu 5000");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
