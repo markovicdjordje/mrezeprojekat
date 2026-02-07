76bae2acc66ecf76d0c7a9b6fd78fee7431c83d1
Djordje <djmarkovic51@gmail.com>
2026-02-07 14:00:22 +0100

    fresh start

--- TaksiVozila/TaksiVozila/Properties/AssemblyInfo.cs
+++ TaksiVozila/TaksiVozila/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("15e48da7-4472-4b70-8415-2a22831801e5")]
+[assembly: Guid("5b204c44-e88f-4828-818b-8ff4963998e6")]
 
 // Version information for an assembly consists of the following four values:
 //
